1052a27de6f884daebe89d0072d716758c75a72b
Darren Cauthon <darren@cauthon.com>
2015-05-11 06:17:49 -0500

    Dumped from the zip file.

---
+++ SpecFlowNullReference/NullReference.feature
@@ -0,0 +1,6 @@
+﻿Feature: NullReference
+
+Scenario: Reproduce NullReferenceException
+	Given I set null value to ScenarioContext
+	When I get value from ScenarioContext
+	Then NullReferenceException is thrown
---
+++ SpecFlowNullReference/NullReferenceSteps.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowNullReference
+{
+    [Binding]
+    public class NullReferenceSteps
+    {
+        private const string Key = "SomeKey";
+        private object value;
+        private NullReferenceException exception;
+
+        [Given(@"I set null value to ScenarioContext")]
+        public void GivenISetNullValueToScenarioContext()
+        {
+            ScenarioContext.Current.Set<object>(null, Key);
+        }
+
+        [When(@"I get value from ScenarioContext")]
+        public void WhenIGetValueFromScenarioContext()
+        {
+            try
+            {
+                value = ScenarioContext.Current.Get<object>(Key);
+            }
+            catch (NullReferenceException e)
+            {
+                exception = e;
+            }
+        }
+
+        [Then(@"NullReferenceException is thrown")]
+        public void ThenNullReferenceExceptionIsThrown()
+        {
+            Assert.IsNull(value);
+            Assert.IsNotNull(exception);
+        }
+    }
+}
